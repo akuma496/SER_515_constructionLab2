1fc33757159a5e94204534c518af8003111e1e35
AdityaKumar-ASU <akuma496@asu.edu>
2025-04-04 22:41:54 -0700

    Acitvity 2 complete

--- VisitorDesignPattern/src/Act2VehicleService.java
+++ VisitorDesignPattern/src/Act2VehicleService.java
@@ -1,3 +1,29 @@
 public class Act2VehicleService {
+    //Goal is to create singleton, using private constructors and static blocks
+    private static Act2VehicleService instance;
+    private IVehicleInspector vehicleInspector;
 
+    private Act2VehicleService(String viProperty) {
+        if (viProperty == null) {
+            throw new IllegalArgumentException("vi property cannot be null");
+        }
+        this.vehicleInspector = VehicleFactory.getVehicleInspector(viProperty);
+    }
+
+    static {
+        String viProp = System.getProperty("vi");
+        instance = new Act2VehicleService(viProp);
+    }
+
+    public static Act2VehicleService getInstance() {
+        return instance;
+    }
+
+    public int calculateTotal(IVehicle[] vehicles) {
+        int total = 0;
+        for (IVehicle vehicle : vehicles) {
+            total += vehicle.accept(vehicleInspector);
+        }
+        return total;
+    }
 }
